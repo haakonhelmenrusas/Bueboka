1eadcda0be2a148f1016bd0123c6a17614922048
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2021-07-23 17:13:10 +0200

    #1 Add Firebase to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import FacebookLogin from './auth/FacebookLogin'
+
 import './index.css';
 import App from './app/App';
 
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FacebookLogin>
+      <App />
+    </FacebookLogin>
   </React.StrictMode>,
   document.getElementById('root')
 );
