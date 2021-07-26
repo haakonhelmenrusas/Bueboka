7356c30854186c94e0a16d50ef6019badef28bd3
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2021-07-26 20:24:28 +0200

    Update FacebookLogin.tsx

--- src/auth/FacebookLogin.tsx
+++ src/auth/FacebookLogin.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import firebase from 'firebase';
 import Button from '../components/button/Button';
 
@@ -11,38 +11,26 @@ const FacebookLogin = () => {
     .auth()
     .signInWithPopup(provider)
     .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      const credential = result.credential;
-  
       // The signed-in user info.
       const user = result.user;
       console.log('FACEBOOK USER: ', result);
-      
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      //const accessToken = credential.accessToken;
-  
-      // ...
     })
     .catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
-      const errorMessage = error.message;
+      //const errorMessage = error.message;
       // The email of the user's account used.
-      const email = error.email;
+      //const email = error.email;
       // The firebase.auth.AuthCredential type that was used.
-      const credential = error.credential;
-  
+      //const credential = error.credential;
       
     })
   }
 
-  useEffect(() => {
-  }, [])
-
   return (
-    <div>
+    <>
       <Button buttonStyle="primary" onClick={loginWithFacebook} label="Logg inn med Facebook"></Button>
-    </div>
+    </>
   )
 
 };
