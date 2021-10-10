299f8712791c111e74647cd4191c1ac74ed28b74
Haakon Helmen <haakon.s.helmen@gmail.com>
2021-10-10 13:38:59 +0200

    Adding conditional rendering of the menu

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,15 +1,21 @@
-import Logo from "../../assets/images/logo512.png";
+import Logo from "../../../assets/images/logo512.png";
 import Navbar from "../navbar/Navbar";
 import styles from "./Header.module.css";
+import {getAuth} from "firebase/auth";
+import firebaseApp from "../../../auth/FirebaseConfig";
 
 const Header = () => {
+  const auth = getAuth(firebaseApp);
+  const userId = auth.currentUser;
   return (
     <header className={styles.header}>
       <div className={styles.brand}>
         <img className={styles.logo} src={Logo} alt="Logo" />
         <h1 className={styles.title}>Book of Arrows</h1>
       </div>
-      <Navbar />
+      {userId && (
+        <Navbar />
+      )}
     </header>
   );
 };
