fbcbd6b58e250f00eef5d1494deb385034724cd1
Haakon Helmen <haakon.s.helmen@gmail.com>
2021-10-10 13:40:41 +0200

    Rename User interface

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,4 +1,4 @@
-export type User = {
+export interface IUser {
   displayName: string;
   email: string;
   photoURL: string;
--- src/types/UserContext.ts
+++ src/types/UserContext.ts
@@ -1,6 +1,6 @@
-import { User } from "./User";
+import { IUser } from "./User";
 
 export type UserContextState = {
-  user: User;
+  user: IUser;
   updateUser: (user: any) => void;
 };
