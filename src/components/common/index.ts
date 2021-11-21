e943a52b27ef36bbf4d92b813d2b96e486f39336
Haakon Helmen <haakon.s.helmen@gmail.com>
2021-11-21 13:34:42 +0100

    Adding footer with copyright and sponsor section

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from './navbar/Navbar';
 export { default as ProfileImage } from './profileImage/ProfileImage';
 export { default as Layout } from './layout/Layout';
+export { default as Footer } from './footer/Footer';
