008ebddf09e69d96ace8480deab5bcb9333ea6bd
Haakon Helmen <haakon.s.helmen@gmail.com>
2022-01-01 16:31:32 +0100

    #10
    Signed-off-by: Haakon Helmen <haakon.s.helmen@gmail.com>

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import styles from "./Navbar.module.css";
+import {Button} from "react-bootstrap";
+import {logOut} from "../../../auth";
 
 const Navbar = () => {
   return (
@@ -12,6 +14,9 @@ const Navbar = () => {
         </li>
         <li className={styles.li}>
           <NavLink to="/form">Sikteskjema</NavLink>
+        </li>
+        <li className={styles.li}>
+          <Button variant="secondary" onClick={logOut}>Logg ut</Button>
         </li>
       </ul>
     </nav>
