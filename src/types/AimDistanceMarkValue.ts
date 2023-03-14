9bedaa03aa51a0227ed84f504aed523fc03c6369
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-03-14 14:40:07 +0100

    Nytt navn på interface

--- src/types/AimDistanceMarkValue.ts
+++
@@ -1,4 +0,0 @@
-export interface AimDistanceMarkValue {
-  aim: number;
-  distance: number;
-}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export type { User } from "./User";
 export type { UserContextState } from "./UserContext";
-export type { AimDistanceMarkValue } from "./AimDistanceMarkValue";
+export type { MarkValue } from "./MarkValue";
 export type { AimDistanceMark } from "./AimDistanceMark";
 export type { CalculatedMarks } from "./CalculatedMarks";
 export type { SightMarkCalculation } from "./SightMarkCalculation";
