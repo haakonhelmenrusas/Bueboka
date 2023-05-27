75dc4fa1280aa0a10dbfe23f841c7e32b1dced38
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-05-27 06:36:17 +0200

    Lagde en egen input komponent som bygger på react native sin

---
+++ components/common/Input.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, TextInput, TextInputProps, View } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+const editableTextInputColor = '#494949';
+
+interface InputProps extends TextInputProps {
+  label: string;
+  placeholderText?: string;
+}
+
+const Input = ({ label, placeholderText, ...props }: InputProps) => {
+  const textInputColor = editableTextInputColor;
+
+  const styles = StyleSheet.create({
+    label: { color: textInputColor, fontWeight: '500', fontSize: 16, marginBottom: 4 },
+    input: {
+      backgroundColor: '#FFF',
+      paddingHorizontal: 12,
+      height: 48,
+      width: '100%',
+      borderColor: textInputColor,
+      borderWidth: 1,
+      borderRadius: 4,
+    },
+  });
+
+  return (
+    <View accessible accessibilityLabel={isAndroid ? label : `${label}${': Disabled!'}`}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={styles.input} placeholder={placeholderText} placeholderTextColor={textInputColor} {...props} />
+    </View>
+  );
+};
+
+export default Input;
