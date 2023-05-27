6bdec885d6b1bc0b688a30b5f6533647ae69d052
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-05-27 06:36:28 +0200

    Første versjon av tabellen

---
+++ app/(tabs)/siktemerker/MarksTable.tsx
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import Button from '../../../components/common/Button';
+import { CalculatedMarks } from '../../../types';
+
+interface CalculationTableProps {
+  ballistics: CalculatedMarks | null;
+  removeMark: (index: number) => void;
+}
+
+export default function MarksTable({ ballistics, removeMark }: CalculationTableProps) {
+  const renderBallisticTable = () => {
+    if (ballistics) {
+      return ballistics.given_distances.map((distance, index) => (
+        <View style={styles.tr} key={index}>
+          <Text>{distance.toFixed(1)}m</Text>
+          <Text>{ballistics.given_marks[index].toFixed(2)}</Text>
+          <Text>{ballistics.calculated_marks[index].toFixed(2)}</Text>
+          <Button label="Fjern" type="outline" onPress={() => removeMark(index)} />
+        </View>
+      ));
+    } else {
+      return <Text>Legg inn siktemerker og send dem inn til beregning</Text>;
+    }
+  };
+
+  return (
+    <View>
+      <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={styles.thead}>Avstand</Text>
+        <Text style={styles.thead}>Merke</Text>
+        <Text style={styles.thead}>Beregnet</Text>
+        <Text style={styles.thead} />
+      </View>
+      {renderBallisticTable()}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tr: {
+    display: 'flex',
+    height: 48,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  td: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  thead: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginTop: 8,
+  },
+});
