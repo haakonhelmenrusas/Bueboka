d326650a6f5a485b6e3ca6f1e8e9cc58208ff440
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-07-19 19:19:46 +0200

    feat: sponsor logo in about page

--- app/(tabs)/omOss/index.tsx
+++ app/(tabs)/omOss/index.tsx
@@ -4,21 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function About() {
   const logo = require('../../../assets/images/arcticBueLogo.png');
 
+  const blurhash =
+    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <Text style={styles.title}>Hei!</Text>
       <Text style={styles.text}>
         Bueboka er en tjeneste for alle bueskyttere. Vi ønsker å gjøre det enklere for alle å finne informasjon om
-        bueskyting. Her kan du bergene din egen score, finne informasjon om konkurranser og finne informasjon om klubber
-        i Norge.
+        bueskyting. Her kan du bergene dine egne siktemerker, finne informasjon om konkurranser og finne informasjon om
+        klubber i Norge.
       </Text>
-      <View style={{ marginTop: 'auto' }}>
-        <Text style={styles.sponsor}>Sponsor</Text>
+
+      <Text style={styles.sponsor}>Sponsor</Text>
+      <View style={styles.sponsorContainer}>
         <Image
           key={logo}
           style={styles.logo}
-          contentFit="cover"
+          contentFit="contain"
           source={logo}
+          transition={500}
+          placeholder={blurhash}
           accessibilityLabel="Arctic Buesport AS Logo"
         />
       </View>
@@ -27,6 +33,10 @@ export default function About() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: 'center',
+  },
   title: {
     color: '#000',
     fontSize: 28,
@@ -35,13 +45,20 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 16,
     marginBottom: 16,
+    textAlign: 'justify',
+  },
+  sponsorContainer: {
+    marginTop: 'auto',
+    width: 200,
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
   sponsor: {
     fontSize: 26,
     textAlign: 'center',
   },
   logo: {
-    width: '100%',
-    height: 180,
+    flex: 1,
   },
 });
