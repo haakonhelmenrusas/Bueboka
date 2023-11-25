2b63f3e9a5dc9de655ef1883d71aa7da1f03df0f
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-11-25 19:10:52 +0100

    new markset interface for storing locally

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,6 @@
-export type { MarkValue } from './MarkValue';
 export type { AimDistanceMark } from './AimDistanceMark';
 export type { CalculatedMarks } from './CalculatedMarks';
+export type { MarkSet } from './MarkSet';
+export type { MarkValue } from './MarkValue';
 export type { SightMarkCalc } from './SightMarkCalc';
 export { Status } from './Status';
