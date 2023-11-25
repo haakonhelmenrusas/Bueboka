488533334e142a26c3df4e32deae2413bebea3e0
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-11-25 15:35:07 +0100

    #37 store mark set in local storage

--- app/(tabs)/siktemerker/index.tsx
+++ app/(tabs)/siktemerker/index.tsx
@@ -1,13 +1,16 @@
 import * as Sentry from '@sentry/react-native';
 import { useEffect, useState } from 'react';
-import { Keyboard, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Keyboard, Modal, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 
+import { Button, Input } from '../../../components/common';
 import { AimDistanceMark, CalculatedMarks, MarkValue } from '../../../types';
 import { Ballistics, getLocalStorage, storeLocalStorage, useBallisticsParams } from '../../../utils/';
 import MarksForm from './components/MarksForm';
 import MarksTable from './components/MarksTable';
 
 export default function Calculate() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [name, setName] = useState('');
   const [ballistics, setBallistics] = useState<CalculatedMarks | null>(null);
   const { error, status, calculateBallisticsParams } = useBallisticsParams();
 
@@ -18,6 +21,19 @@ export default function Calculate() {
       }
     });
   }, []);
+
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const handleSave = () => {
+    storeMarksWithName(name);
+    closeModal();
+  };
 
   async function sendMarks(newMark: MarkValue) {
     const body: AimDistanceMark = {
@@ -50,6 +66,24 @@ export default function Calculate() {
     await sendMarks({ aim: 9999, distance: newDistances[0] });
   }
 
+  async function storeMarksWithName(name: string) {
+    if (ballistics) {
+      const marksSet = {
+        name: name,
+        marks: ballistics.given_marks,
+        distances: ballistics.given_distances,
+      };
+
+      try {
+        // Store marks set in local storage
+        await storeLocalStorage(marksSet, name);
+        setBallistics(null);
+      } catch (error) {
+        Sentry.captureException(error);
+      }
+    }
+  }
+
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={{ flex: 1 }}>
@@ -61,6 +95,33 @@ export default function Calculate() {
           </>
         )}
         <MarksTable ballistics={ballistics} removeMark={handleRemoveMark} />
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 'auto' }}>
+          <Button
+            label="Fjern merker"
+            type="outline"
+            onPress={() => {
+              storeLocalStorage(null, 'ballistics').then(() => {
+                setBallistics(null);
+              });
+            }}
+          />
+          <Button label="Opprett sett" type="filled" onPress={() => openModal()} />
+        </View>
+        <Modal visible={modalVisible} animationType="fade">
+          <View style={{ flex: 1, margin: 32, justifyContent: 'center', alignItems: 'center' }}>
+            <Input
+              label="Sett navn på settet"
+              error={false}
+              onChangeText={setName}
+              value={name}
+              placeholder="Enter name"
+            />
+            <View style={{ flexDirection: 'row', marginTop: 24, justifyContent: 'space-between' }}>
+              <Button type="outline" label="Avbryt" onPress={closeModal} />
+              <Button label="Lagre sett" onPress={handleSave} />
+            </View>
+          </View>
+        </Modal>
       </View>
     </TouchableWithoutFeedback>
   );
