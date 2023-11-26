a5c564f17ca76e08e2cb53ef8c61fa72bff2e31f
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-11-26 12:00:31 +0100

    adding sentry env

--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,7 @@ Sentry.init({
   dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
   enableInExpoDevelopment: true,
   debug: false,
+  environment: process.env.EXPO_PUBLIC_SENTRY_ENVIRONMENT,
 });
 
 const Index = () => {
