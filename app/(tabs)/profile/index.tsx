c8277fd6d311244ad924acf6a606d5c5f059b658
Haakon Helmen Rusås <haakon.helmen.rusas@intility.no>
2024-06-10 21:50:25 +0200

    fix: clean up

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,11 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Profile() {
-  const logo = require('../../../assets/images/arcticBueLogo.png');
-
-  const blurhash =
-    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hei skytter!</Text>
