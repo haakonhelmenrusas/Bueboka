fcf6b3877d72253b38980a004a75770e9faf80af
Haakon Helmen Rusås <haakon.helmen.rusas@intility.no>
2024-07-07 18:52:35 +0200

    fix: remove unused code

--- app/(tabs)/siktemerker/components/index.ts
+++ app/(tabs)/siktemerker/components/index.ts
@@ -1,6 +1,5 @@
 export { default as ConfirmRemoveMarks } from './ConfirmRemoveMarks';
 export { default as MarksForm } from './MarksForm';
 export { default as MarksTable } from './MarksTable';
-export { default as SetModal } from './SetModal';
 export { useCalcForm } from './useCalcForm';
 export { useCalcMarksForm } from './useCalcMarksForm';
