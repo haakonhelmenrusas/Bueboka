7fa8a0062537afd874918ebde9cf3294aa0abd1c
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2024-11-08 11:00:00 +0100

    ci: init Sentry
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,9 @@
 import { Stack } from 'expo-router';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://310ad5a856229859c003cf549b110334@o4505578901929984.ingest.us.sentry.io/4508262003048448',
+});
 
 export default function RootLayout() {
   return (
