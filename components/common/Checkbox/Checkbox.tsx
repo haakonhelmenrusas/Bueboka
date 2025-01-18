b31718e8b755f5c7d5ba9e95d0871c6c364f4521
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2025-01-18 10:30:27 +0100

    new arch (#118)
    * Remove old unused webstorm config files
    
    * fix: edit prettier config
    
    * ci: upgrade typescript
    
    * ci: upgrade babel, testing-library
    
    * ci: upgrade fontawesome
    
    * ci: update prettier config
    
    * ci: upgrade packages
    
    * ci: expo fix dep
    
    * fix: refactor marks form to submit single mark-distance
    
    * ci: fix eslint and prettier
    
    * fix: refactor out function to helpers dir
    
    * fix: add comment for storing marks to local storage
    
    * fix: Fixing dept
    
    * config: update eslint rules
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * chore: adding test for bow form hook
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: adding better type safety
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: removing unused code
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: checking type before passing to method
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * refactor: adding test and moving logic out
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: use the new checkDecimal func
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: sonar fixes
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: adding more tests
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: type safety
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * docs: update readme with project info
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: trying to reset android project
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: clean project
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: reset after upgrading to SDK 52
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * fix: new arch enabled
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * ci: adding build number back to app file
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * docs: adding some docs to the method
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * docs: add owner prop
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * ci: update expo and packages
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    * refactor: adding test for checkbox
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
    
    ---------
    
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>

--- components/common/Checkbox/Checkbox.tsx
+++ components/common/Checkbox/Checkbox.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 interface CheckboxProps {
@@ -10,9 +10,14 @@ interface CheckboxProps {
 
 export default function Checkbox({ label, checked, onPress }: Readonly<CheckboxProps>) {
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity onPress={onPress} testID="TouchableOpacity">
       <View style={styles.container}>
-        <Feather name={checked ? 'check-square' : 'square'} size={24} color={checked ? '#007AFF' : '#000'} />
+        <Feather
+          name={checked ? 'check-square' : 'square'}
+          size={24}
+          color={checked ? '#007AFF' : '#000'}
+          testID={checked ? 'Feather__check-square' : 'Feather__square'}
+        />
         <Text style={styles.label}>{label}</Text>
       </View>
     </TouchableOpacity>
