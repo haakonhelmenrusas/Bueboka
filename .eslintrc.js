99b18214249bbf4c90e4691be584fcdc554229e0
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2025-01-24 09:23:55 +0100

    config: fix eslint
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+  ignorePatterns: ['/dist/*'],
+};
