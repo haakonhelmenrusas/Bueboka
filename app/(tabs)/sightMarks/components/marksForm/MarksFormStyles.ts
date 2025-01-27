8767bf547f6a1d1f9a666cea1afa2ca41b6fa9e4
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2025-01-27 14:21:36 +0100

    style: placement of the marks form
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>

--- app/(tabs)/sightMarks/components/marksForm/MarksFormStyles.ts
+++ app/(tabs)/sightMarks/components/marksForm/MarksFormStyles.ts
@@ -1,9 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-end',
+    marginBottom: Platform.OS === 'ios' ? 32 : 0,
   },
   form: {
     display: 'flex',
