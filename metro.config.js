a224ca1c4d7e70664bf7e0d3134d733bb0a89776
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2025-01-27 13:00:13 +0100

    ci: setup react-native-reanimated package
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,8 @@
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
 const config = getSentryExpoConfig(__dirname);
 
 config.resolver.blockList = [/(.*.test.ts?)$/];
 
-module.exports = config;
+module.exports = wrapWithReanimatedMetroConfig(config);
