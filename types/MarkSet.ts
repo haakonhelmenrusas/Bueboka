0bd8870ab109e8d87ee6969e0dee33cecb957b06
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2025-03-24 07:09:10 +0100

    chore: add detailed interfaces for archery marks and values
    Signed-off-by: Haakon Helmen Rusås <haakon.s.helmen@gmail.com>

--- types/MarkSet.ts
+++ types/MarkSet.ts
@@ -1,5 +1,13 @@
+/**
+ * Interface representing a set of marks for archery.
+ */
 export interface MarkSet {
+  /** The name of the mark set. */
   name: string;
+
+  /** Array of marks corresponding to the distances. */
   marks: number[];
+
+  /** Array of distances corresponding to the marks. */
   distances: number[];
 }
--- types/Status.ts
+++ types/Status.ts
@@ -1,6 +1,5 @@
 export enum Status {
   Idle = 'idle',
   Pending = 'pending',
-  Success = 'success',
   Error = 'error',
 }
