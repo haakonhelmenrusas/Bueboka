942d031ccd4c5e33998579110750de37ac2fb355
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-06-30 06:48:27 +0200

    feat: new arrow card

---
+++ components/profile/arrowCard/ArrowCard.tsx
@@ -0,0 +1,48 @@
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { ArrowSet } from '@/types';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons/faCog';
+import { styles } from './ArrowCardStyles';
+import { colors } from '@/styles/colors';
+
+interface Props {
+  arrowSet: ArrowSet;
+  openFormWithData: () => void;
+}
+
+export default function ArrowCard({ arrowSet, openFormWithData }: Props) {
+  const { name, spine, weight, length, material } = arrowSet;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require('@/assets/bow.png')} style={[styles.image, { tintColor: colors.tertiary }]} />
+        <Text style={styles.title}>{name}</Text>
+        <TouchableOpacity onPress={openFormWithData} style={styles.cogIcon}>
+          <FontAwesomeIcon icon={faCog} size={16} color={colors.white} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.column}>
+          <View>
+            <Text style={styles.head}>Spine</Text>
+            <Text style={styles.text}>{spine ? spine : 'Ingen data'}</Text>
+          </View>
+          <View>
+            <Text style={styles.head}>Vekt</Text>
+            <Text style={styles.text}>{weight ? weight : 'Ingen data'}</Text>
+          </View>
+        </View>
+        <View style={styles.column}>
+          <View>
+            <Text style={styles.head}>Material</Text>
+            <Text style={styles.text}>{material}</Text>
+          </View>
+          <View>
+            <Text style={styles.head}>Lengde</Text>
+            <Text style={styles.text}>{length ? length + ' cm' : 'Ingen data'}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
---
+++ components/profile/arrowCard/ArrowCardStyles.ts
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '@/styles/colors';
+
+export const styles = StyleSheet.create({
+    container: {
+        alignContent: 'center',
+        width: '100%',
+        marginHorizontal: 'auto',
+        backgroundColor: colors.white,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: colors.primary,
+    },
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        alignItems: 'center',
+        borderTopEndRadius: 8,
+        borderTopStartRadius: 8,
+        backgroundColor: colors.primary,
+    },
+    image: {
+        width: 20,
+        height: 20,
+        resizeMode: 'contain',
+        marginRight: 16,
+    },
+    title: {
+        fontSize: 20,
+        color: colors.tertiary,
+        fontWeight: 'medium',
+    },
+    cogIcon: {
+        marginLeft: 'auto',
+        padding: 4,
+    },
+    body: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 16,
+    },
+    column: {
+        flex: 1,
+    },
+    head: {
+        fontSize: 16,
+        fontWeight: '600',
+        marginBottom: 4,
+    },
+    text: {
+        fontSize: 14,
+        marginBottom: 16,
+    },
+});
