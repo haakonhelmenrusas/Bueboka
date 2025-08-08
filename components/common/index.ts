4b1e8ed75a33285f1738411c16224ce608249a6f
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-08-08 10:34:24 +0200

    feat: new toggle component - toggle with label and animation
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- components/common/index.ts
+++ components/common/index.ts
@@ -4,5 +4,6 @@ import Message from './Message/Message';
 import Notch from './Notch/Notch';
 import { Select } from './Select/Select';
 import ModalWrapper from './ModalWrapper/ModalWrapper';
+import Toggle from './Toggle/Toggle';
 
-export { Button, Input, Message, Notch, Select, ModalWrapper };
+export { Button, Input, Message, Notch, Select, ModalWrapper, Toggle };
