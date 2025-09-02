5e406eb172c75d349fa36ba3fd29a7077fcb807d
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-09-02 09:52:37 +0200

    refactor: remove unused jest cleanup hooks in `jestSetup.ts`
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- jestSetup.ts
+++ jestSetup.ts
@@ -1,12 +1,1 @@
 jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
-// Clear all timers after each test
-afterEach(() => {
-  jest.clearAllTimers();
-  jest.runOnlyPendingTimers();
-});
-
-// Clear all mocks after all tests
-afterAll(() => {
-  jest.clearAllMocks();
-  jest.restoreAllMocks();
-});
