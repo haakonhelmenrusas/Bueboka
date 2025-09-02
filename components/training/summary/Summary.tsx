2de8bb1b8ab4be52c743e364c41b9dca599c1ffa
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-09-02 13:11:57 +0200

    refactor: format JSX layout for improved readability in Summary component
    - Adjusted nested `View` layouts for consistent indentation.
    - Unified quotation style across imports and component strings.
    
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- components/training/summary/Summary.tsx
+++ components/training/summary/Summary.tsx
@@ -1,7 +1,7 @@
-import { View, Text } from "react-native";
-import { styles } from "./SummaryStyles";
-import { Training } from "@/types";
-import { sumArrows } from "../helpers/sumArrows";
+import { Text, View } from 'react-native';
+import { styles } from './SummaryStyles';
+import { Training } from '@/types';
+import { sumArrows } from '../helpers/sumArrows';
 
 interface SummaryProps {
   trainings: Training[];
@@ -10,9 +10,18 @@ export default function Summary({ trainings }: SummaryProps) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Oppsummering</Text>
-      <View style={styles.textLine}><Text style={styles.subtitle}>Siste 7 dagene </Text> <Text style={styles.arrowCount}> {sumArrows(trainings, "7days")} </Text></View>
-      <View style={styles.textLine}><Text style={styles.subtitle}>Denne måneden </Text> <Text style={styles.arrowCount}> {sumArrows(trainings, "month")} </Text></View>
-      <View style={styles.textLine}><Text style={styles.subtitle}>Totalt </Text> <Text style={styles.arrowCount}> {sumArrows(trainings)} </Text></View>
+      <View style={styles.textLine}>
+        <Text style={styles.subtitle}>Siste 7 dagene</Text>
+        <Text style={styles.arrowCount}>{sumArrows(trainings, '7days')}</Text>
+      </View>
+      <View style={styles.textLine}>
+        <Text style={styles.subtitle}>Denne måneden</Text>
+        <Text style={styles.arrowCount}>{sumArrows(trainings, 'month')}</Text>
+      </View>
+      <View style={styles.textLine}>
+        <Text style={styles.subtitle}>Totalt</Text>
+        <Text style={styles.arrowCount}>{sumArrows(trainings)}</Text>
+      </View>
     </View>
   );
 }
