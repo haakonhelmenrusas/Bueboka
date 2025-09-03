5e97864e641fd41f420c33a6fdb4c3459a587e96
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-09-03 20:18:31 +0200

    feat: setting sight marks tab in focus if marks data exist
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- app/(tabs)/sightMarks/index.tsx
+++ app/(tabs)/sightMarks/index.tsx
@@ -1,11 +1,31 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import CalculateScreen from '@/components/sightMarks/screens/CalculateScreen';
 import MarksScreen from '@/components/sightMarks/screens/MarksScreen';
 import { styles } from '@/components/sightMarks/SightMarksStyles';
+import { getLocalStorage } from '@/utils';
+import { MarksResult } from '@/types';
+import { useFocusEffect } from 'expo-router';
 
 export default function SightMarks() {
   const [screen, setScreen] = useState('calculate');
+
+  const loadData = useCallback(async () => {
+    try {
+      const calculatedMarksData = await getLocalStorage<MarksResult>('calculatedMarks');
+      if (calculatedMarksData) {
+        setScreen('marks');
+      }
+    } catch (error) {
+      console.error('Error loading data:', error);
+    }
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [loadData]),
+  );
 
   return (
     <View style={styles.container}>
