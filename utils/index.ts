04d04d8410f3ddb67e2f89aaa66156f2f79bf093
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2026-02-17 11:38:10 +0100

    refactor: remove unused hooks from index export
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,5 @@
 export { Ballistics } from './Constants';
 export { capitalizeFirstLetter } from './helpers/capitalizeFirstLetter';
-export { default as useBallisticsParams } from './hooks/useBallisticsParams';
-export { default as useCalculateMarks } from './hooks/useCalculateMarks';
 export { handleNumberChange } from './helpers/handleNumberChange';
 export { checkDecimalCount } from './helpers/handleNumberChange';
 export { sortItems } from './helpers/sortItems';
