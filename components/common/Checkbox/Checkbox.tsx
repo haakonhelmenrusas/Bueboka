083ff89eccddcb408eb6ddd7b1ec6a35f8c1fb87
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2026-03-07 09:03:28 +0100

    feat: add Checkbox component with styles and accessibility features
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

---
+++ components/common/Checkbox/Checkbox.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AccessibilityRole, AccessibilityState, Pressable, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+import { colors } from '@/styles/colors';
+import { styles } from './CheckboxStyles';
+
+interface CheckboxProps {
+  value: boolean;
+  onChange: (newValue: boolean) => void;
+  label?: string;
+}
+
+export default function Checkbox({ value, onChange, label }: CheckboxProps) {
+  const accessibilityState: AccessibilityState = {
+    checked: value,
+  };
+
+  return (
+    <Pressable
+      style={styles.wrapper}
+      onPress={() => onChange(!value)}
+      accessibilityRole={'checkbox' as AccessibilityRole}
+      accessibilityState={accessibilityState}
+      accessibilityLabel={label || 'Checkbox'}>
+      <View style={[styles.checkbox, value && styles.checkboxChecked]}>
+        {value && <FontAwesomeIcon icon={faCheck} size={14} color={colors.white} />}
+      </View>
+      {label ? <Text style={styles.label}>{label}</Text> : <View />}
+    </Pressable>
+  );
+}
--- components/common/index.ts
+++ components/common/index.ts
@@ -6,9 +6,24 @@ import Notch from './Notch/Notch';
 import { Select } from './Select/Select';
 import ModalWrapper from './ModalWrapper/ModalWrapper';
 import Toggle from './Toggle/Toggle';
+import Checkbox from './Checkbox/Checkbox';
 import DataValue from './DataValue/DataValue';
 import ModalHeader from './ModalHeader/ModalHeader';
 import DatePicker from './DatePicker/DatePicker';
 import { OfflineBanner } from './OfflineBanner/OfflineBanner';
 
-export { Button, Input, Textarea, DatePicker, Message, Notch, Select, ModalWrapper, Toggle, DataValue, ModalHeader, OfflineBanner };
+export {
+  Button,
+  Input,
+  Textarea,
+  DatePicker,
+  Message,
+  Notch,
+  Select,
+  ModalWrapper,
+  Toggle,
+  Checkbox,
+  DataValue,
+  ModalHeader,
+  OfflineBanner,
+};
