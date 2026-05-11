86752c6938d21c64e5823224fa8d97e651f1480b
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2026-05-11 11:28:40 +0200

    fix: jest was pointing to old root expo-modules-core folder
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,6 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|better-auth|@better-auth|@sentry)',
   ],
   moduleNameMapper: {
-    '^expo-modules-core(.*)$': '<rootDir>/node_modules/expo/node_modules/expo-modules-core$1',
+    '^expo-modules-core(.*)$': '<rootDir>/node_modules/expo-modules-core$1',
   },
 };
